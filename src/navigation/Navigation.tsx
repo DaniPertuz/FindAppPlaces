46f6bb708837b21caf729fa63ff2cbdbcc5315eb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-16 22:19:56 -0500

    Update main stack navigation

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,13 +2,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { LoginScreen, RegisterScreen, RegisterDetailsScreen, MainPictureScreen } from '../screens/auth';
 import { MainScreen } from '../screens/main';
+import NewPasswordScreen from '../screens/auth/NewPasswordScreen';
 
 const Stack = createStackNavigator();
 
 export type RootStackParams = {
     RegisterDetailsScreen: { name: string, email: string; },
     MainPictureScreen: undefined,
-    MainScreen: undefined;
+    MainScreen: undefined,
+    NewPasswordScreen: undefined,
+    RegisterScreen: undefined,
 };
 
 export const Navigation = () => {
@@ -22,6 +25,7 @@ export const Navigation = () => {
             }}
         >
             <Stack.Screen name='LoginScreen' component={LoginScreen} />
+            <Stack.Screen name='NewPasswordScreen' component={NewPasswordScreen} />
             <Stack.Screen name='RegisterScreen' component={RegisterScreen} />
             <Stack.Screen name='RegisterDetailsScreen' component={RegisterDetailsScreen} />
             <Stack.Screen name='MainPictureScreen' component={MainPictureScreen} />
