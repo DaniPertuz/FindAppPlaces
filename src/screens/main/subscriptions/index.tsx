af6f611c123b926784e827f645a512eb68302487
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-19 22:19:40 -0500

    Subscription screen

---
+++ src/screens/main/subscriptions/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const SubscriptionScreen = () => {
+    return (
+        <Text>SubscriptionScreen</Text>
+    );
+};
+
+export default SubscriptionScreen;
