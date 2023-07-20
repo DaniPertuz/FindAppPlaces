8eb651c85b810d65cfbd3b2e5a614361ca305676
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-19 22:15:16 -0500

    Add NewPasswordScreen to index file

--- src/screens/auth/index.ts
+++ src/screens/auth/index.ts
@@ -2,3 +2,4 @@ export { default as LoginScreen } from './LoginScreen';
 export { default as MainPictureScreen } from './MainPictureScreen';
 export { default as RegisterScreen } from './RegisterScreen';
 export { default as RegisterDetailsScreen } from './RegisterDetailsScreen';
+export { default as NewPasswordScreen } from './NewPasswordScreen';
