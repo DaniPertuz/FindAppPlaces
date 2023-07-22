08dc9946d16092f7d6c267920da280b0c3333cfe
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-22 00:19:29 -0500

    Add ProfileNavigator to MainNavigator

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ProfileNavigator } from './';
 import BottomTabNavigator from './BottomTabNavigator';
 
 export type RootStackParams = {
-    BottomTabNavigator: undefined
+    BottomTabNavigator: undefined;
     LoginScreen: undefined,
     MainScreen: undefined,
     NewPasswordScreen: undefined,
+    Profile: undefined,
     RegisterScreen: undefined,
 };
 
@@ -14,12 +16,9 @@ const Stack = createStackNavigator<RootStackParams>();
 
 const MainNavigator = () => {
     return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false,
-            }}
-        >
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="BottomTabNavigator" component={BottomTabNavigator} />
+            <Stack.Screen name="Profile" component={ProfileNavigator} />
         </Stack.Navigator>
     );
 };
