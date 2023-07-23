adbbf6c05a1ff25d915746f20d86c7d21cc66aed
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-23 13:43:12 -0500

    Add UpdateProfileScreen to main stack navigation

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import { ProfileNavigator } from './';
 import BottomTabNavigator from './BottomTabNavigator';
+import { IPlace } from '../interfaces';
+import { UpdateProfileScreen } from '../screens';
 
 export type RootStackParams = {
     BottomTabNavigator: undefined;
@@ -10,6 +13,7 @@ export type RootStackParams = {
     NewPasswordScreen: undefined,
     Profile: undefined,
     RegisterScreen: undefined,
+    UpdateProfileScreen: { place: IPlace}
 };
 
 const Stack = createStackNavigator<RootStackParams>();
@@ -19,6 +23,7 @@ const MainNavigator = () => {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="BottomTabNavigator" component={BottomTabNavigator} />
             <Stack.Screen name="Profile" component={ProfileNavigator} />
+            <Stack.Screen name="UpdateProfileScreen" component={UpdateProfileScreen} />
         </Stack.Navigator>
     );
 };
