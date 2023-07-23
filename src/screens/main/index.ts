0e4aaf06580a052830f4af9152db70fbbbf6ccd3
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-23 13:42:00 -0500

    UpdateProfileScreen component

--- src/screens/main/index.ts
+++ src/screens/main/index.ts
@@ -1,3 +1,4 @@
 export { default as MainScreen } from './MainScreen';
 export { default as SubscriptionScreen } from './subscriptions';
 export { default as ProfileScreen } from './profile';
+export { default as UpdateProfileScreen } from './profile/UpdateProfileScreen';
---
+++ src/screens/main/profile/UpdateProfileScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const UpdateProfileScreen = () => {
+    return (
+        <Text>UpdateProfileScreen</Text>
+    );
+};
+
+export default UpdateProfileScreen;
