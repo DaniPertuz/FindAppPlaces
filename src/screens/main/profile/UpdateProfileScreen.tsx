0fcb5c69e670e25e734bd11d3a2c64dbd5e449c8
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-30 01:16:11 -0500

    Screen design update

--- src/screens/main/profile/UpdateProfileScreen.tsx
+++ src/screens/main/profile/UpdateProfileScreen.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 
-const UpdateProfileScreen = () => {
+import UpdateProfileInputs from '../../../components/UpdateProfileInputs';
+import { useIcons } from '../../../hooks';
+import { RootStackParams } from '../../../navigation/MainNavigator';
+
+import styles from '../../../themes/AppTheme';
+
+interface Props extends StackScreenProps<RootStackParams, 'UpdateProfileScreen'> { };
+
+const UpdateProfileScreen = ({ route, navigation }: Props) => {
+
+    const { place } = route.params;
+
     return (
-        <Text>UpdateProfileScreen</Text>
+        <View style={{ marginHorizontal: 25, marginTop: 50 }}>
+            <View style={{ ...styles.flexDirectionRow, ...styles.smallMediumMarginBottom }}>
+                <View style={styles.flexOneAlignJustifyCenter}>
+                    <TouchableOpacity
+                        activeOpacity={1.0}
+                        onPress={() => navigation.goBack()}
+                    >
+                        {useIcons('Back', 20, 20)}
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flex: 200, ...styles.alignJustifyCenter }}>
+                    <Text style={styles.captionTwoBlack}>Editar Perfil</Text>
+                </View>
+            </View>
+            <View style={{ marginHorizontal: 15 }}>
+                <UpdateProfileInputs place={place} />
+            </View>
+        </View>
     );
 };
 
