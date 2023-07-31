3fe471249c84d21cd1d8fad60c8c5bbc208f4ca6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-30 23:15:35 -0500

    Styles rules, usePlace hook and PremiumLevel component

--- src/screens/main/MainScreen.tsx
+++ src/screens/main/MainScreen.tsx
@@ -1,14 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import { Image, ScrollView, Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import LinearGradient from 'react-native-linear-gradient';
 
-import { AuthContext, PlacesContext } from '../../context';
-import { useIcons } from '../../hooks';
-import { IPlace, IRatingList } from '../../interfaces';
+import { usePlace } from '../../hooks';
 import { RootStackParams } from '../../navigation/MainNavigator';
 import BigButtons from '../../components/BigButtons';
+import PremiumLevel from '../../components/PremiumLevel';
 
 import styles from '../../themes/AppTheme';
 
@@ -16,100 +14,24 @@ const MainScreen = () => {
 
     const navigator = useNavigation<StackNavigationProp<RootStackParams>>();
 
-    const { user } = useContext(AuthContext);
-    const { getFavorites, getRatings, loadPlaceByEmail } = useContext(PlacesContext);
-
-    const [place, setPlace] = useState<IPlace>();
-    const [ratings, setRatings] = useState<IRatingList>({ total: 0, rates: [] });
-    const [favorites, setFavorites] = useState<number>(0);
-
-    const getPlaceFavorites = async () => {
-        const favorites = await getFavorites(place?._id!);
-        return favorites;
-    };
-
-    const getPlaceInfo = async () => {
-        const place = await loadPlaceByEmail(user?.email!);
-        return place;
-    };
-
-    const getPlaceRatings = async () => {
-        const ratings = await getRatings(place?._id!);
-        return ratings;
-    };
-
-    useEffect(() => {
-        let mounted = true;
-        getPlaceInfo().then((data) => {
-            if (mounted) {
-                setPlace(data);
-            }
-        });
-        return () => {
-            mounted = false;
-        };
-    }, []);
-
-    useEffect(() => {
-        let mounted = true;
-        if (place) {
-            getPlaceFavorites().then((data) => {
-                if (mounted) {
-                    setFavorites(data);
-                }
-            });
-        }
-        return () => {
-            mounted = false;
-        };
-    }, [place]);
-
-    useEffect(() => {
-        let mounted = true;
-        if (place) {
-            getPlaceRatings().then((data) => {
-                if (mounted) {
-                    setRatings(data);
-                }
-            });
-        }
-        return () => {
-            mounted = false;
-        };
-    }, [place]);
+    const { favorites, place, ratings } = usePlace();
 
     return (
-        <View style={{ paddingHorizontal: 20, paddingTop: 63 }}>
+        <View style={styles.mainContainer}>
             <View style={styles.flexDirectionRowJustifySpaceBetween}>
                 <View>
                     <Text style={styles.subheadline}>Bienvenid@</Text>
                     <View style={{ marginVertical: 3 }}>
-                        <Text numberOfLines={1} style={styles.bodySmall}>{user?.name}</Text>
+                        <Text numberOfLines={1} style={styles.bodySmall}>{place?.name}</Text>
                     </View>
-
-                    <LinearGradient
-                        colors={(place?.premium === 3) ? ['#D6B238', '#F6E074'] : (place?.premium === 2) ? ['#B8B8B8', '#E2E2E2'] : ['#C08E5E', '#FAC294']}
-                        start={{ x: 0, y: 0 }}
-                        end={{ x: 1, y: 0 }}
-                        style={{
-                            alignItems: 'center',
-                            borderRadius: 999,
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            paddingHorizontal: 10,
-                            paddingVertical: 3
-                        }}
-                    >
-                        {useIcons('Trophy', 22, 22)}
-                        <Text style={styles.footnote}>Nivel {place?.premium}</Text>
-                    </LinearGradient>
+                    <PremiumLevel place={place!} />
                 </View>
                 <TouchableOpacity
                     activeOpacity={1.0}
                     onPress={() => navigator.navigate('Profile')}
                 >
                     <Image
-                        source={(user?.photo === '' ? require('../../assets/fa_blue.png') : { uri: user?.photo })}
+                        source={(place?.photo === '' ? require('../../assets/fa_blue.png') : { uri: place?.photo })}
                         style={{ borderRadius: 32, height: 62, width: 62 }}
                     />
                 </TouchableOpacity>
