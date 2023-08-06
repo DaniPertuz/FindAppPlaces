98da5a67a47c511d47bd1297510db746c660913b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-08-05 22:39:27 -0500

    No records validation

--- src/components/KeywordsChart.tsx
+++ src/components/KeywordsChart.tsx
@@ -15,7 +15,7 @@ const KeywordsChart = ({ services }: Props) => {
 
     const unique = services.filter((a, i) => services.findIndex((s) => a.search === s.search) === i);
 
-    const data = unique.map((item, index) => (
+    const data: any[] = unique.map((item, index) => (
         {
             name: item.search,
             amount: services.filter(service => service.search === item.search).length,
@@ -30,40 +30,49 @@ const KeywordsChart = ({ services }: Props) => {
             <View style={styles.mediumPaddingHorizontal}>
                 <Text style={styles.subheadline}>Usuarios frecuentes</Text>
             </View>
-            <View style={{ ...styles.mediumMarginTop, ...styles.alignItemsBaseline }}>
-                <PieChart
-                    data={data}
-                    hasLegend={false}
-                    backgroundColor={"transparent"}
-                    paddingLeft={"20"}
-                    absolute
-                    center={[75, 0]}
-                    accessor='amount'
-                    width={320}
-                    height={220}
-                    chartConfig={{
-                        backgroundGradientFrom: "rgba(250, 250, 250, 0.98)",
-                        backgroundGradientTo: "rgba(250, 250, 250, 0.98)",
-                        color: () => '#207CFD',
-                        labelColor: () => '#858585',
-                    }}
-                />
-                <View style={styles.mediumPaddingHorizontal}>
-                    <View style={{ ...styles.flexDirectionRowAlignCenter, flexWrap: 'wrap' }}>
-                        {data.map((d, index) => (
-                            <View key={index} style={{ ...styles.flexDirectionRowAlignCenter, margin: 10 }}>
-                                <View style={{ backgroundColor: colors[index], borderRadius: 25, height: 25, width: 25 }} />
-                                <View style={styles.mediumMarginStart}>
-                                    <Text style={styles.footnoteGray}>{d.amount}</Text>
-                                </View>
-                                <View style={styles.mediumMarginStart}>
-                                    <Text style={styles.footnoteGray}>{d.name}</Text>
-                                </View>
-                            </View>
-                        ))}
+            {(data.length === 0)
+                ?
+                <View style={{ ...styles.mediumMarginTop, ...styles.alignItemsCenter }}>
+                    <View style={styles.statisticsZeroMessage}>
+                        <Text style={styles.caption}>Esperando búsquedas</Text>
                     </View>
                 </View>
-            </View>
+                :
+                <View style={{ ...styles.mediumMarginTop, ...styles.alignItemsBaseline }}>
+                    <PieChart
+                        data={data}
+                        hasLegend={false}
+                        backgroundColor={"transparent"}
+                        paddingLeft={"20"}
+                        absolute
+                        center={[75, 0]}
+                        accessor='amount'
+                        width={320}
+                        height={220}
+                        chartConfig={{
+                            backgroundGradientFrom: "rgba(250, 250, 250, 0.98)",
+                            backgroundGradientTo: "rgba(250, 250, 250, 0.98)",
+                            color: () => '#207CFD',
+                            labelColor: () => '#858585',
+                        }}
+                    />
+                    <View style={styles.mediumPaddingHorizontal}>
+                        <View style={{ ...styles.flexDirectionRowAlignCenter, flexWrap: 'wrap' }}>
+                            {data.map((d, index) => (
+                                <View key={index} style={{ ...styles.flexDirectionRowAlignCenter, margin: 10 }}>
+                                    <View style={{ backgroundColor: colors[index], borderRadius: 25, height: 25, width: 25 }} />
+                                    <View style={styles.mediumMarginStart}>
+                                        <Text style={styles.footnoteGray}>{d.amount}</Text>
+                                    </View>
+                                    <View style={styles.mediumMarginStart}>
+                                        <Text style={styles.footnoteGray}>{d.name}</Text>
+                                    </View>
+                                </View>
+                            ))}
+                        </View>
+                    </View>
+                </View>
+            }
         </View>
     );
 };
--- src/components/UsersChart.tsx
+++ src/components/UsersChart.tsx
@@ -5,7 +5,7 @@ import styles from '../themes/AppTheme';
 
 interface Props {
     favorites: number;
-    history: number
+    history: number;
 }
 
 const UsersChart = ({ favorites, history }: Props) => {
@@ -21,49 +21,60 @@ const UsersChart = ({ favorites, history }: Props) => {
         }
     ];
 
+    const zeros = data.map((item) => item.amount === 0);
+
     return (
         <View style={styles.chartContainer}>
             <View style={styles.mediumPaddingHorizontal}>
                 <Text style={styles.subheadline}>Usuarios frecuentes</Text>
             </View>
-            <View style={{ ...styles.mediumMarginTop, ...styles.alignItemsBaseline }}>
-                <PieChart
-                    data={data}
-                    hasLegend={false}
-                    backgroundColor={"transparent"}
-                    paddingLeft={"20"}
-                    absolute
-                    center={[75, 0]}
-                    accessor='amount'
-                    width={320}
-                    height={220}
-                    chartConfig={{
-                        backgroundGradientFrom: "rgba(250, 250, 250, 0.98)",
-                        backgroundGradientTo: "rgba(250, 250, 250, 0.98)",
-                        color: () => '#207CFD',
-                        labelColor: () => '#858585',
-                    }}
-                />
-                <View style={styles.flexDirectionRowAlignSelfCenter}>
-                    <View style={styles.legendItemContainer}>
-                        <View style={{ backgroundColor: '#207CFD', ...styles.legendItemColor }} />
-                        <View style={styles.mediumMarginStart}>
-                            <Text style={styles.footnoteGray}>{history}</Text>
-                        </View>
-                        <View style={styles.mediumMarginStart}>
-                            <Text style={styles.footnoteGray}>Historial</Text>
-                        </View>
+            <View style={{ ...styles.mediumMarginTop, ...styles.alignItemsCenter }}>
+                {(zeros[0] === true && zeros[1] === true)
+                    ?
+                    <View style={styles.statisticsZeroMessage}>
+                        <Text style={styles.caption}>Esperando usuarios visitantes</Text>
                     </View>
-                    <View style={styles.legendItemContainer}>
-                        <View style={{ backgroundColor: '#58D7FE', ...styles.legendItemColor }} />
-                        <View style={styles.mediumMarginStart}>
-                            <Text style={styles.footnoteGray}>{favorites}</Text>
+                    :
+                    <>
+                        <PieChart
+                            data={data}
+                            hasLegend={false}
+                            backgroundColor={"transparent"}
+                            paddingLeft={"20"}
+                            absolute
+                            center={[60, 0]}
+                            accessor='amount'
+                            width={320}
+                            height={220}
+                            chartConfig={{
+                                backgroundGradientFrom: "rgba(250, 250, 250, 0.98)",
+                                backgroundGradientTo: "rgba(250, 250, 250, 0.98)",
+                                color: () => '#207CFD',
+                                labelColor: () => '#858585',
+                            }}
+                        />
+                        <View style={styles.flexDirectionRowAlignSelfCenter}>
+                            <View style={styles.legendItemContainer}>
+                                <View style={{ backgroundColor: '#207CFD', ...styles.legendItemColor }} />
+                                <View style={styles.mediumMarginStart}>
+                                    <Text style={styles.footnoteGray}>{history}</Text>
+                                </View>
+                                <View style={styles.mediumMarginStart}>
+                                    <Text style={styles.footnoteGray}>Historial</Text>
+                                </View>
+                            </View>
+                            <View style={styles.legendItemContainer}>
+                                <View style={{ backgroundColor: '#58D7FE', ...styles.legendItemColor }} />
+                                <View style={styles.mediumMarginStart}>
+                                    <Text style={styles.footnoteGray}>{favorites}</Text>
+                                </View>
+                                <View style={styles.mediumMarginStart}>
+                                    <Text style={styles.footnoteGray}>Favoritos</Text>
+                                </View>
+                            </View>
                         </View>
-                        <View style={styles.mediumMarginStart}>
-                            <Text style={styles.footnoteGray}>Favoritos</Text>
-                        </View>
-                    </View>
-                </View>
+                    </>
+                }
             </View>
         </View>
     );
