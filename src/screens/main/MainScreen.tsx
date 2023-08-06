a372bd6339aa808728572e6dec890d5051d2e311
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-08-05 22:03:01 -0500

    Render depending on data loading

--- src/screens/main/MainScreen.tsx
+++ src/screens/main/MainScreen.tsx
@@ -10,6 +10,7 @@ import PremiumLevel from '../../components/PremiumLevel';
 import KeywordsChart from '../../components/KeywordsChart';
 import SearchChart from '../../components/SearchChart';
 import UsersChart from '../../components/UsersChart';
+import LoadingScreen from '../LoadingScreen';
 
 import styles from '../../themes/AppTheme';
 
@@ -20,39 +21,45 @@ const MainScreen = () => {
     const { favorites, history, place, ratings } = usePlace();
 
     return (
-        <View style={styles.mainContainer}>
-            <View style={styles.flexDirectionRowJustifySpaceBetween}>
-                <View>
-                    <Text style={styles.subheadline}>Bienvenid@</Text>
-                    <View style={styles.tinyMarginVertical}>
-                        <Text numberOfLines={1} style={styles.bodySmall}>{place?.name}</Text>
+        <>
+            {!place && <LoadingScreen />}
+
+            {place &&
+                <View style={styles.mainContainer}>
+                    <View style={styles.flexDirectionRowJustifySpaceBetween}>
+                        <View>
+                            <Text style={styles.subheadline}>Bienvenid@</Text>
+                            <View style={styles.tinyMarginVertical}>
+                                <Text numberOfLines={1} style={styles.bodySmall}>{place?.name}</Text>
+                            </View>
+                            <PremiumLevel place={place!} />
+                        </View>
+                        <TouchableOpacity
+                            activeOpacity={1.0}
+                            onPress={() => navigator.navigate('Profile')}
+                        >
+                            <Image
+                                source={(place?.photo === '' ? require('../../assets/fa_blue.png') : { uri: place?.photo })}
+                                style={styles.placePhoto}
+                            />
+                        </TouchableOpacity>
                     </View>
-                    <PremiumLevel place={place!} />
+                    <View style={styles.mediumMarginTop}>
+                        <BigButtons category={place?.category!} favorites={favorites.total} rate={Number(place?.rate?.$numberDecimal!)} ratings={ratings} />
+                    </View>
+                    <View style={styles.mediumMarginTop}>
+                        <Text style={styles.subheadline}>Estadísticas</Text>
+                    </View>
+                    <View style={styles.mediumPaddingVertical}>
+                        <ScrollView showsVerticalScrollIndicator={false} contentInset={{ bottom: 450 }}>
+                            <SearchChart />
+                            <UsersChart favorites={favorites.total} history={history.total} />
+                            <KeywordsChart services={history.services} />
+                        </ScrollView>
+                    </View>
                 </View>
-                <TouchableOpacity
-                    activeOpacity={1.0}
-                    onPress={() => navigator.navigate('Profile')}
-                >
-                    <Image
-                        source={(place?.photo === '' ? require('../../assets/fa_blue.png') : { uri: place?.photo })}
-                        style={styles.placePhoto}
-                    />
-                </TouchableOpacity>
-            </View>
-            <View style={styles.mediumMarginTop}>
-                <BigButtons category={place?.category!} favorites={favorites.total} rate={Number(place?.rate?.$numberDecimal!)} ratings={ratings} />
-            </View>
-            <View style={styles.mediumMarginTop}>
-                <Text style={styles.subheadline}>Estadísticas</Text>
-            </View>
-            <View style={styles.mediumPaddingVertical}>
-                <ScrollView showsVerticalScrollIndicator={false} contentInset={{ bottom: 450 }}>
-                    <SearchChart />
-                    <UsersChart favorites={favorites.total} history={history.total} />
-                    <KeywordsChart />
-                </ScrollView>
-            </View>
-        </View>
+            }
+        </>
     );
 };
 
