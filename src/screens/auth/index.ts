b9175001db7a950c97b2b2502ba527fb9450b8ec
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-08-14 23:52:33 -0500

    Remove unused screen

--- src/screens/auth/index.ts
+++ src/screens/auth/index.ts
@@ -1,4 +1,3 @@
 export { default as LoginScreen } from './LoginScreen';
-export { default as MainPictureScreen } from './MainPictureScreen';
 export { default as RegisterScreen } from './RegisterScreen';
 export { default as NewPasswordScreen } from './NewPasswordScreen';
