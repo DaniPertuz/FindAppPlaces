6736b5ea352ac57ba47c7eb1c1af28e1f3852704
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-12-28 23:30:24 -0500

    Add hooks to index file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,7 @@
 export * from './useCoords';
+export * from './useEmailValidation';
+export * from './useFieldValidation';
 export * from './useForm';
 export * from './useIcons';
+export * from './usePasswordVisibility';
 export * from './usePlace';
