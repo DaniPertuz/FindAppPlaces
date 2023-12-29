678ccfe7ceaf4ffdaae3235fa328eecd664bb84c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-12-28 23:35:43 -0500

    Add StatusBarComponent and refactor code

--- src/screens/auth/RegisterScreen.tsx
+++ src/screens/auth/RegisterScreen.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native';
 
 import { useForm } from '../../hooks/useForm';
 import Background from '../../components/Background';
 import RegisterFormInputs from '../../components/RegisterFormInputs';
+import StatusBarComponent from '../../components/StatusBarComponent';
 
 import styles from '../../themes/AppTheme';
 
 const RegisterScreen = () => {
+
+    const [resize, setResize] = useState<string>('');
 
     const { name, email, password, onChange } = useForm({
         name: '',
@@ -15,20 +18,23 @@ const RegisterScreen = () => {
         password: ''
     });
 
+    const handleResize = (resize: string) => {
+        setResize(resize);
+    };
+
     return (
         <ScrollView keyboardShouldPersistTaps='handled' style={styles.scrollViewBackground}>
+            <StatusBarComponent color='#081023' theme='light-content' />
             <Background />
             <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? 'padding' : 'height'}>
-                <View style={styles.loginFormContainer}>
-                    <View style={styles.alignItemsCenter}>
-                        <View style={styles.largeMarginTop}>
-                            <View style={styles.mediumMarginBottom}>
-                                <View style={styles.tinyMarginBottom}>
-                                    <Text style={styles.h4}>Crea tu cuenta</Text>
-                                </View>
-                                <Text style={styles.bodySmall}>Ingresa tus datos para crear una cuenta</Text>
-                                <RegisterFormInputs name={name} email={email.trim()} password={password} onChange={onChange} />
+                <View style={[styles.registerFormContainer, (resize !== '') && { paddingBottom: resize }]}>
+                    <View style={styles.largeMarginTop}>
+                        <View style={styles.mediumMarginBottom}>
+                            <View style={styles.tinyMarginBottom}>
+                                <Text style={styles.h4}>Crea tu cuenta</Text>
                             </View>
+                            <Text style={styles.bodySmall}>Ingresa tus datos para crear una cuenta</Text>
+                            <RegisterFormInputs name={name} email={email.trim()} password={password} onChange={onChange} handleResize={handleResize} />
                         </View>
                     </View>
                 </View>
