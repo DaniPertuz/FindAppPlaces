b73668f453123a9cf7b9757250dfd311b086b2d6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-12-28 23:28:28 -0500

    useEmailValidation hook

---
+++ src/hooks/useEmailValidation.ts
@@ -0,0 +1,7 @@
+const useEmailValidation = (email: string): boolean => {
+  const emailRegexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+  return emailRegexp.test(email);
+};
+
+export default useEmailValidation;
