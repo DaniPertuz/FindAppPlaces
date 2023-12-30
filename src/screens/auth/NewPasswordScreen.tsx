1883f1f5dbfd2bd5b981c96ed3199f6e0b5c4bd1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-12-29 23:04:12 -0500

    Refactor code

--- src/screens/auth/NewPasswordScreen.tsx
+++ src/screens/auth/NewPasswordScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { KeyboardAvoidingView, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
@@ -16,41 +16,35 @@ const NewPasswordScreen = () => {
 
     const navigator = useNavigation<StackNavigationProp<RootStackParams>>();
 
-    const [resize, setResize] = useState<string>('');
-
     const { email, password, confirmPassword, onChange } = useForm({
         email: '',
         password: '',
         confirmPassword: ''
     });
 
-    const handleResize = (resize: string) => {
-        setResize(resize);
-    };
-
     return (
         <ScrollView keyboardShouldPersistTaps='handled' style={styles.scrollViewBackground}>
             <StatusBarComponent color='#081023' theme='light-content' />
             <Background />
-            <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? 'padding' : 'height'}>
-                <View style={[styles.newPasswordFormContainer, (resize !== '') && { paddingBottom: resize }]}>
-                    <View style={styles.titleMarginTopContainer}>
+            <View style={styles.newPasswordFormContainer}>
+                <View style={styles.titleMarginTopContainer}>
+                    <TouchableOpacity
+                        activeOpacity={1.0}
+                        style={styles.backButtonMargins}
+                        onPress={() => navigator.replace('LoginScreen')}
+                    >
                         <View style={styles.flexDirectionRow}>
-                            <TouchableOpacity
-                                activeOpacity={1.0}
-                                style={styles.backButtonMargins}
-                                onPress={() => navigator.replace('LoginScreen')}
-                            >
+                            <View style={{ paddingTop: 2 }}>
                                 {useIcons('Back', 20, 20)}
-                            </TouchableOpacity>
+                            </View>
                             <Text style={styles.backButtonText}>Volver</Text>
                         </View>
-                        <Text style={styles.h4}>Crear nueva contraseña</Text>
-                        <Text style={styles.bodySmall}>Ingresa tu nueva contraseña</Text>
-                    </View>
-                    <NewPasswordFormInputs email={email.trim()} password={password} confirmPassword={confirmPassword} onChange={onChange} handleResize={handleResize} />
+                    </TouchableOpacity>
+                    <Text style={styles.h4}>Crear nueva contraseña</Text>
+                    <Text style={styles.bodySmall}>Ingresa tu nueva contraseña</Text>
                 </View>
-            </KeyboardAvoidingView>
+                <NewPasswordFormInputs email={email.trim()} password={password} confirmPassword={confirmPassword} onChange={onChange} />
+            </View>
         </ScrollView>
     );
 };
--- src/screens/auth/RegisterScreen.tsx
+++ src/screens/auth/RegisterScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 import { useForm } from '../../hooks/useForm';
 import Background from '../../components/Background';
@@ -10,35 +10,27 @@ import styles from '../../themes/AppTheme';
 
 const RegisterScreen = () => {
 
-    const [resize, setResize] = useState<string>('');
-
     const { name, email, password, onChange } = useForm({
         name: '',
         email: '',
         password: ''
     });
 
-    const handleResize = (resize: string) => {
-        setResize(resize);
-    };
-
     return (
         <ScrollView keyboardShouldPersistTaps='handled' style={styles.scrollViewBackground}>
             <StatusBarComponent color='#081023' theme='light-content' />
             <Background />
-            <KeyboardAvoidingView behavior={(Platform.OS === 'ios') ? 'padding' : 'height'}>
-                <View style={[styles.registerFormContainer, (resize !== '') && { paddingBottom: resize }]}>
-                    <View style={styles.largeMarginTop}>
-                        <View style={styles.mediumMarginBottom}>
-                            <View style={styles.tinyMarginBottom}>
-                                <Text style={styles.h4}>Crea tu cuenta</Text>
-                            </View>
-                            <Text style={styles.bodySmall}>Ingresa tus datos para crear una cuenta</Text>
-                            <RegisterFormInputs name={name} email={email.trim()} password={password} onChange={onChange} handleResize={handleResize} />
+            <View style={styles.registerFormContainer}>
+                <View style={styles.largeMarginTop}>
+                    <View style={styles.mediumMarginBottom}>
+                        <View style={styles.tinyMarginBottom}>
+                            <Text style={styles.h4}>Crea tu cuenta</Text>
                         </View>
+                        <Text style={styles.bodySmall}>Ingresa tus datos para crear una cuenta</Text>
+                        <RegisterFormInputs name={name} email={email.trim()} password={password} onChange={onChange} />
                     </View>
                 </View>
-            </KeyboardAvoidingView>
+            </View>
         </ScrollView>
     );
 };
