bb82c28c870850ec0e9311b0c948a15449817cb2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-01-11 22:00:39 -0500

    Password Input Update Profile component and styles

---
+++ src/components/profile/forms/password-input/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TextInput, TouchableOpacity, View } from 'react-native';
+import { useIcons, usePasswordVisibility } from '../../../../hooks';
+import { styles } from './styles';
+
+interface Props {
+    field: string;
+    fieldValue: 'password' | 'confirmPassword';
+    placeholder: string;
+    onChange: (value: string, field: 'password' | 'confirmPassword') => void;
+}
+
+const PasswordInput = ({ field, fieldValue, placeholder, onChange }: Props) => {
+
+    const { eyeIcon, passwordVisibility, handlePasswordVisibility } = usePasswordVisibility();
+
+    return (
+        <View style={styles.inputFieldContainerWhite}>
+            <View style={styles.tinyButtonSize}>{useIcons('Lock', 20, 20)}</View>
+            <TextInput
+                autoCapitalize='none'
+                autoCorrect={false}
+                onChangeText={(value) => onChange(value, fieldValue)}
+                placeholder={placeholder}
+                placeholderTextColor='#9A9A9A'
+                secureTextEntry={passwordVisibility}
+                selectionColor='#9A9A9A'
+                style={[styles.inputField, styles.flexTwo]}
+                value={field}
+            />
+            <TouchableOpacity activeOpacity={1.0} onPress={handlePasswordVisibility}>
+                <View style={styles.alignItemsCenter}>{useIcons(eyeIcon, 20, 20)}</View>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default PasswordInput;
---
+++ src/components/profile/forms/password-input/styles.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    alignItemsCenter: {
+        alignItems: 'center'
+    },
+    flexTwo: {
+        flex: 2
+    },
+    inputField: {
+        color: '#081023',
+        fontSize: 14,
+        height: 60,
+        letterSpacing: -0.24,
+        lineHeight: 16,
+        padding: 12
+    },
+    inputFieldContainerWhite: {
+        alignItems: 'center',
+        backgroundColor: 'rgba(250, 250, 250, 1)',
+        borderColor: '#081023',
+        borderRadius: 8,
+        borderWidth: 1,
+        flexDirection: 'row',
+        marginTop: 5,
+        paddingHorizontal: 16
+    },
+    tinyButtonSize: {
+        flex: 0.2
+    }
+});
