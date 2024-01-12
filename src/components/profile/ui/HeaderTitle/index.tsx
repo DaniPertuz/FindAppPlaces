d3da16d478035e7ba0d62546a1362c55be28483c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-01-11 21:39:55 -0500

    Update Profile's HeaderTitle component and styles

---
+++ src/components/profile/ui/HeaderTitle/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { styles } from './styles';
+
+const HeaderTitle = () => <Text style={styles.captionTwoBlack}>Editar Perfil</Text>;
+
+export default HeaderTitle;
---
+++ src/components/profile/ui/HeaderTitle/styles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    captionTwoBlack: {
+        color: '#1F273A',
+        fontSize: 14,
+        fontWeight: '500',
+        lineHeight: 20,
+        letterSpacing: -0.24
+    }
+});
