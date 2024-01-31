23569524eec5e4923ad230869a0c122c5e95a4cb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-01-30 22:53:37 -0500

    General styles for profile section

---
+++ src/screens/main/profile/styles.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    containerMargins: {
+        marginHorizontal: 15
+    },
+    flexOneAlignJustifyCenter: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center'
+    },
+    headerContainer: {
+        flexDirection: 'row',
+        marginBottom: 10
+    },
+    headerAlignment: {
+        flex: 200,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    mainContainer: {
+        marginHorizontal: 20,
+        marginTop: 40
+    }
+});
