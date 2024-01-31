52a370535974993c2a31fc087dcec6d6e7666ca1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-01-30 22:52:38 -0500

    General styles for update-profile section

---
+++ src/components/profile/update-profile/styles.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    containerPaddingBottom: {
+        paddingBottom: 100
+    },
+    flexDirectionRowJustifySpaceBetween: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    imageFromGallery: {
+        borderRadius: 8,
+        height: 100,
+        marginEnd: 15,
+        width: 100
+    },
+    minusButton: {
+        alignSelf: 'flex-end',
+        marginBottom: -10,
+        marginEnd: 5,
+        zIndex: 999
+    },
+    mediumMarginTop: {
+        marginTop: 20
+    }
+});
