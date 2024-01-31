1dfcd4eb5f4f12ee080fa06b79eec9c85cd6a7f4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-01-30 22:57:21 -0500

    Profile ImagesGallery component and styles

---
+++ src/components/profile/forms/images-gallery/styles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    flexDirectionRow: {
+        flexDirection: 'row'
+    },
+    flexDirectionRowJustifySpaceBetween: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+});
