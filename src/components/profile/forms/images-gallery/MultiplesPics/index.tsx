adf30def01e34eb59490ad91658bbbc397ea4ecc
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-04 02:11:23 -0500

    ImagesGallery MultiplePics component

---
+++ src/components/profile/forms/images-gallery/MultiplesPics/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Image, ScrollView, View } from 'react-native';
+import AddImageButton from '../AddImageButton';
+import EditButton from '../../../../ui/EditButton';
+import { IPlace } from '../../../../../interfaces';
+import { styles } from './styles';
+
+interface Props {
+    editPics: boolean;
+    place: IPlace;
+    placeImages: string[];
+    addGalleryPics: () => void;
+    onPressEdit: () => void;
+}
+
+const MultiplePics = ({ editPics, place, placeImages, addGalleryPics, onPressEdit }: Props) => {
+    return (
+        <>
+            {(editPics)
+                ?
+                <View style={styles.flexDirectionRow}>
+                    <AddImageButton onPress={addGalleryPics} icon="CameraPlus" />
+                </View>
+                :
+                <>
+                    <View style={styles.flexDirectionRow}>
+                        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                            {placeImages.map((image, index) => (
+                                <View key={index}>
+                                    <Image source={{ uri: image }} style={styles.imageFromGallery} />
+                                </View>
+                            ))}
+                            {(placeImages.length === 0 && (place.premium === 2 || place.premium === 3)) && <AddImageButton onPress={addGalleryPics} icon="CameraPlus" />}
+                        </ScrollView>
+                    </View>
+                    {(placeImages.length !== 0) && <EditButton onPress={onPressEdit} />}
+                </>
+            }
+        </>
+    );
+};
+
+export default MultiplePics;
