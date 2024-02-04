46088a383b1de500cb3f7f0a3ba3cc0666db746c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-04 02:11:01 -0500

    ImagesGallery SinglePic component

---
+++ src/components/profile/forms/images-gallery/SinglePic/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Image, View } from 'react-native';
+import AddImageButton from '../AddImageButton';
+import EditButton from '../../../../ui/EditButton';
+import { styles } from './styles';
+
+interface Props {
+    editPics: boolean;
+    uri: string;
+    addGalleryPic: () => void;
+    onPressEdit: () => void;
+}
+
+const SinglePic = ({ editPics, uri, addGalleryPic, onPressEdit}: Props) => {
+    return (
+        <>
+            {(editPics)
+                ?
+                <View style={styles.flexDirectionRow}>
+                    <AddImageButton onPress={addGalleryPic} icon="CameraPlus" />
+                </View>
+                :
+                <>
+                    <View style={styles.flexDirectionRow}>
+                        <Image source={{ uri }} style={styles.imageFromGallery} />
+                    </View>
+                    <EditButton onPress={onPressEdit} />
+                </>
+            }
+        </>
+    );
+};
+
+export default SinglePic;
---
+++ src/components/profile/forms/images-gallery/SinglePic/styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    flexDirectionRow: {
+        flexDirection: 'row'
+    },
+    imageFromGallery: {
+        borderRadius: 8,
+        height: 100,
+        marginEnd: 15,
+        width: 100
+    }
+});
