3fc3b88de2de046f9062b9250a6bf4e0d6be6572
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-04 02:17:08 -0500

    Refactor component

--- src/components/profile/forms/images-gallery/index.tsx
+++ src/components/profile/forms/images-gallery/index.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { ImagePickerResponse } from 'react-native-image-picker';
 
-import AddImageButton from './AddImageButton';
-import { UpdateProfilePlaceImages, UpdateProfileRemoveImage } from '../../update-profile';
+import SinglePic from './SinglePic';
+import MultiplePics from './MultiplesPics';
+
 import { useImagesGallery } from '../../../../hooks';
 import { IPlace } from '../../../../interfaces';
 
@@ -15,117 +16,30 @@ interface Props {
 }
 
 const ImagesGallery = ({ place, handlePicsFromGallery }: Props) => {
-    const {
-        displayCamera,
-        displayCameraOne,
-        displayCameraTwo,
-        placeImage,
-        placeImageOne,
-        placeImages,
-        placeImageTwo,
-        response,
-        addGalleryPic,
-        addGalleryPics,
-        handleRemovePic,
-        removeImageOne,
-        removeImageOneFromAllImages,
-        removeImageTwo,
-        removeImageTwoFromAllImages,
-        removeSingleImage,
-        setDisplayCameraOne,
-        setDisplayCameraTwo,
-        setPlaceImage,
-        setPlaceImageOne,
-        setPlaceImageTwo
-    } = useImagesGallery({ place });
+    const { placeImages, response, addGalleryPic, addGalleryPics } = useImagesGallery({ place });
+
+    const [editPics, setEditPics] = useState(false);
+
+    const onPressEdit = () => {
+        setEditPics(true);
+    };
 
     useEffect(() => {
-        if (response) handlePicsFromGallery(response);
+        if (response) {
+            handlePicsFromGallery(response);
+            setEditPics(false);
+        }
     }, [response]);
 
     return (
-        <>
-            {(!place.pics) ?
-                <AddImageButton onPress={() => addGalleryPics()} icon="CameraPlus" />
+        <View style={styles.containerMarginTop}>
+            {(place.premium === 1)
+                ?
+                <SinglePic addGalleryPic={addGalleryPic} editPics={editPics} onPressEdit={onPressEdit} uri={placeImages[0]} />
                 :
-                <>
-                    {(place.premium === 1) &&
-                        <>
-                            {(placeImages.length === 1)
-                                ?
-                                <>
-                                    <UpdateProfileRemoveImage uri={placeImages[0]!} onRemove={removeSingleImage} />
-                                    {(displayCamera) &&
-                                        <View>
-                                            <AddImageButton onPress={() => addGalleryPic(setPlaceImage)} icon="CameraPlus" />
-                                        </View>
-                                    }
-                                </>
-                                :
-                                <View style={styles.flexDirectionRow}>
-                                    {(placeImage === '')
-                                        ?
-                                        <AddImageButton onPress={() => addGalleryPic(setPlaceImage)} icon="CameraPlus" />
-                                        :
-                                        <UpdateProfileRemoveImage uri={placeImage} onRemove={() => setPlaceImage('')} />
-                                    }
-                                </View>
-                            }
-                        </>
-                    }
-                    {(place.premium === 2) &&
-                        <>
-                            {(placeImages.length === 2) ?
-                                <View style={styles.flexDirectionRow}>
-                                    <View>
-                                        {(placeImages[0] !== '') &&
-                                            <UpdateProfileRemoveImage uri={placeImages[0]!} onRemove={removeImageOneFromAllImages} />
-                                        }
-                                        {(placeImageOne !== '') &&
-                                            <UpdateProfileRemoveImage uri={placeImageOne} onRemove={removeImageOne} />
-                                        }
-                                    </View>
-                                    {(displayCameraOne && (placeImages[0] === '' || placeImageOne === '')) &&
-                                        <AddImageButton onPress={() => addGalleryPic(setPlaceImage, setDisplayCameraOne)} icon="CameraPlus" />
-                                    }
-                                    <View>
-                                        {(placeImages[1] !== '') &&
-                                            <UpdateProfileRemoveImage uri={placeImages[1]!} onRemove={removeImageTwoFromAllImages} />
-                                        }
-                                        {(placeImageTwo !== '') &&
-                                            <UpdateProfileRemoveImage uri={placeImageTwo} onRemove={removeImageTwo} />
-                                        }
-                                    </View>
-                                    {(displayCameraTwo) &&
-                                        <View>
-                                            <AddImageButton onPress={() => addGalleryPic(setPlaceImage, setDisplayCameraTwo)} icon="CameraPlus" />
-                                        </View>
-                                    }
-                                </View>
-                                :
-                                <View style={styles.flexDirectionRow}>
-                                    {(placeImageOne === '')
-                                        ?
-                                        <AddImageButton onPress={() => addGalleryPic(setPlaceImage, setDisplayCameraOne)} icon="CameraPlus" />
-                                        :
-                                        <UpdateProfileRemoveImage uri={placeImageOne} onRemove={() => setPlaceImageOne('')} />
-                                    }
-                                    {(placeImageTwo === '')
-                                        ?
-                                        <AddImageButton onPress={() => addGalleryPic(setPlaceImage, setDisplayCameraTwo)} icon="CameraPlus" />
-                                        :
-                                        <UpdateProfileRemoveImage uri={placeImageTwo} onRemove={() => setPlaceImageTwo('')} />
-                                    }
-                                </View>
-                            }
-                        </>
-                    }
-                    {(place.premium === 3) &&
-                        <UpdateProfilePlaceImages images={placeImages} onAdd={() => addGalleryPics()} onRemove={handleRemovePic} />
-                    }
-                </>
+                <MultiplePics addGalleryPics={addGalleryPics} editPics={editPics} onPressEdit={onPressEdit} place={place} placeImages={placeImages} />
             }
-        </>
+        </View>
     );
 };
 
