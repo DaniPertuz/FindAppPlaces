559b0018ab965e7a9786be64e49d60335de450df
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-02-26 10:26:24 -0500

    Remove export of deleted components

--- src/components/profile/update-profile/index.ts
+++ src/components/profile/update-profile/index.ts
@@ -1,4 +1,1 @@
 export { default as UpdateProfileInputs } from './UpdateProfileInputs';
-export { default as UpdateProfileMultipleImages } from './UpdateProfileMultipleImages';
-export { default as UpdateProfilePlaceImages } from './UpdateProfilePlaceImages';
-export { default as UpdateProfileRemoveImage } from './UpdateProfileRemoveImage';
