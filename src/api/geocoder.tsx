3b28903041282a4eb817e598b265dd18091e2ed6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-03-04 22:48:52 -0500

    Update import for geocoder

--- src/api/geocoder.tsx
+++ src/api/geocoder.tsx
@@ -1,6 +1,6 @@
 import Geocoder from 'react-native-geocoding';
-import { GOOGLE_MAPS_API_KEY } from '@env';
-
-Geocoder.init(GOOGLE_MAPS_API_KEY);
+// import { GOOGLE_MAPS_API_KEY } from '@env';
+const GOOGLE_MAPS_API_KEY = process.env['GOOGLE_MAPS_API_KEY'];
+Geocoder.init(GOOGLE_MAPS_API_KEY!);
 
 export default Geocoder;
